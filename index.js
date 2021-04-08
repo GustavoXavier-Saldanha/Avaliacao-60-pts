6c7f647d8dc77fc123565414a44da0c6472e368b
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-04-08 10:20:09 -0300

    Início programação - declaração de var

---
+++ index.js
@@ -0,0 +1,65 @@
+//  1- Perguntar o nome da pessoa;
+
+// 2- Perguntar o tamanho da pizza;
+
+// 3- Perguntar quantos sabores serão a pizza;
+//     3.1- perguntar o/os sabores das pizzas;
+
+// 4- perguntar se ele quer borda recheada;
+
+// 5- Perguntar a bebida ;
+
+// 6- tipo de entrega;
+//     6.1- se for delivery pedir os dados de localização(Celular, Rua, Número, Complemento, Bairro, Ponto de referência);
+
+// 7- Fazer a soma de todos os valores;
+
+// 8- Confirmar o pedido;
+
+// 9- Forma de pagamento;
+//     9.1- se for em dinheiro tem que ver se precisa de troco;
+
+// 10- nomear o número do pedido;
+
+// 11- mostrar valores na tela;
+
+// 12- Agradecemos sua preferência senhor(a) Fulano de tal.
+
+let nome = prompt("Bem vindo, por favor informe seu nome : )!");
+
+let tamanhoPizza = parseInt(prompt(`${nome} 1: Pequena 20cm  - R$25,00\n2: Media 25cm  - R$30,00\n3: Gigante 35cm  - R$40,00\n4: Familia 40cm  - R$45,00` ));
+
+let bordaPizza = parseInt(prompt(`Tipo de borda:\n1: Gorgonzola  - R$10,00\n2: Provolone  - R$8,00\n3:Catupiry  - R$5,00\n4: Cheddar - R$5,00\n5:Sem borda`));
+
+let numeroSaborPizza = parseInt(prompt(`${nome} você deseja uma pizza com 1 ou 2 sabores?`));
+
+let saborPizza = parseInt(prompt(`Calabresa - Mussarela, Calabresa e cebola \n
+4 Queijos - Mussarela, Cheddar, Parmesão e Prato\n
+Frango com Catupiry- Mussarela, Frango e Catupiry\n
+Portuguesa - Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n
+Camarão - Mussarela, Camarão, Salsinha e Cebolinha\n
+Carne seca - Mussarela, Carne seca e cream cheese\n
+Lombo Canadense - Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n
+X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita\n
+`));
+
+let saborPizza2 = parseInt(prompt(`Calabresa - Mussarela, Calabresa e cebola \n
+4 Queijos - Mussarela, Cheddar, Parmesão e Prato\n
+Frango com Catupiry- Mussarela, Frango e Catupiry\n
+Portuguesa - Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n
+Camarão - Mussarela, Camarão, Salsinha e Cebolinha\n
+Carne seca - Mussarela, Carne seca e cream cheese\n
+Lombo Canadense - Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n
+X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita\n
+`));
+
+let bebida    
+let tipoEntrega 
+let dadosEntrega
+let somaTotal
+let confirmacao
+let formaPagamento
+
+
+
+
