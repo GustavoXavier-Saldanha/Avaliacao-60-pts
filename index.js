b30b25acdea6fa7a6ae8a9f6ad55bd85c516a098
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-04-08 16:43:50 -0300

    parti 1 - feita

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 // 6- tipo de entrega;
 //     6.1- se for delivery pedir os dados de localização(Celular, Rua, Número, Complemento, Bairro, Ponto de referência);
 
-// 7- Fazer a soma de todos os valores;
+// ok 7- Fazer a soma de todos os valores;
 
 // 8- Confirmar o pedido;
 
@@ -27,63 +27,254 @@
 
 let nome = prompt("Bem vindo, por favor informe seu nome : )!");
 
-let tamanhoPizza = parseInt(prompt(`${nome} 1: Pequena 20cm  - R$25,00\n2: Media 25cm  - R$30,00\n3: Gigante 35cm  - R$40,00\n4: Familia 40cm  - R$45,00`));
+let telefone;
+let enderecoCompleto;
+let complemento;
+let bairro;
+let referencia;
+let nomeRetirada;
+let telefoneRetirada;
+let troco;
+let valorTroco;
+
+let tamanhoPizza = parseInt(prompt(`Beleza,${nome}!!\nAgora escolha o tamanho da sua pizza:\n1: Pequena 20cm  - R$25,00\n2: Media 25cm  - R$30,00\n3: Gigante 35cm  - R$40,00\n4: Familia 40cm  - R$45,00`));
 let valorTamanhoPizza = [25, 30, 40, 45];
 
-let bordaPizza = parseInt(prompt(`Tipo de borda:\n1: Gorgonzola  - R$10,00\n2: Provolone  - R$8,00\n3:Catupiry  - R$5,00\n4: Cheddar - R$5,00\n5:Sem borda`));
+let bordaPizza = parseInt(prompt(`Agora escolha o tipo de borda:\n1: Gorgonzola  - R$10,00\n2: Provolone  - R$8,00\n3:Catupiry  - R$5,00\n4: Cheddar - R$5,00\n5: Sem borda recheada`));
 let valorBordaPizza = [10, 8, 5, 5, 0];
 
 let numeroSaborPizza = parseInt(prompt(`${nome} você deseja uma pizza com 1 ou 2 sabores?`));
 
-let saborPizza = parseInt(prompt(`1: Calabresa - Mussarela, Calabresa e cebola \n2: 4 Queijos - Mussarela, Cheddar, Parmesão e Prato\n3: Frango com Catupiry- Mussarela, Frango e Catupiry\n4: Portuguesa - Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n5: Camarão - Mussarela, Camarão, Salsinha e Cebolinha\n6: Carne seca - Mussarela, Carne seca e cream cheese\n7: Lombo Canadense - Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n8: X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita
-`));
-let nomeSabor = ["Calabresa", "4 Queijos", "Frango com Catupiry", "Portuguesa", "Camarão", "Carne seca", "Lombo Canadense", "X-Fritas"
-];
 
+let saborPizza = parseInt(prompt(`Perfeito, nós temos os seguintes sabores:\n1: Calabresa - Mussarela, Calabresa e cebola \n2: 4 Queijos - Mussarela, Cheddar, Parmesão e Prato\n3: Frango com Catupiry- Mussarela, Frango e Catupiry\n4: Portuguesa - Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n5: Camarão - Mussarela, Camarão, Salsinha e Cebolinha\n6: Carne seca - Mussarela, Carne seca e cream cheese\n7: Lombo Canadense - Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n8: X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita
+`));
+
+let saborPizza2;
 
 if (numeroSaborPizza == 2) {
-
-    let saborPizza2 = parseInt(prompt(`Agora o segundo sabor:\n1: Calabresa - Mussarela, Calabresa e cebola \n2: 4 Queijos - Mussarela, Cheddar, Parmesão e Prato\n3: Frango com Catupiry- Mussarela, Frango e Catupiry\n4: Portuguesa - Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n5: Camarão - Mussarela, Camarão, Salsinha e Cebolinha\n6: Carne seca - Mussarela, Carne seca e cream cheese\n7: Lombo Canadense - Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n8: X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita
+    saborPizza2 = parseInt(prompt(`Agora escolha o sabor da outra metade:\n1: Calabresa - Mussarela, Calabresa e cebola \n2: 4 Queijos - Mussarela, Cheddar, Parmesão e Prato\n3: Frango com Catupiry- Mussarela, Frango e Catupiry\n4: Portuguesa - Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n5: Camarão - Mussarela, Camarão, Salsinha e Cebolinha\n6: Carne seca - Mussarela, Carne seca e cream cheese\n7: Lombo Canadense - Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n8: X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita
 `));
-    let nomeSabor2 = ["Calabresa", "4 Queijos", "Frango com Catupiry", "Portuguesa", "Camarão", "Carne seca", "Lombo Canadense", "X-Fritas"
-    ];
 }
 
-let bebida = parseInt(prompt(`1: Coca Cola  - R$12,00\n2: Fanta - R$10,00\n3: Guaraná Antarctica  - R$10,00\n4: H2O  - R$8,00\n5: Del Valle  - R$7,00`));
-let valorBebida = [12, 10, 10, 8, 7];
 
-let tipoEntrega = parseInt(prompt(`1: Retirada no Balcão - R$0,00\n
-2: Delivery  - R$5,00
-`));
+let bebida = parseInt(prompt(`Agora para complementar o lanche, escolha uma bebida.\nNós temos as seguintes opções:\n1: Coca Cola  - R$12,00\n2: Fanta - R$10,00\n3: Guaraná Antarctica  - R$10,00\n4: H2O  - R$8,00\n5: Del Valle  - R$7,00\n6: Sem bebida`));
+let valorBebida = [12, 10, 10, 8, 7, 0];
+
+let tipoEntrega = parseInt(prompt(`Qual é a forma de entrega que você prefere?\n1: Retirada no Balcão - R$0,00\n
+2: Delivery  - R$5,00\nLembrando que se for retirar o pedido no balcão, sempre de máscara.`));
 let valorEntrega = [0, 5];
-
-if(tipoEntrega == 2){
-    let dadosEntregaDelivery = {
-        celular: prompt(`Digite o seu número de celular`),
-        bairro: prompt(`Digite o nome do seu bairro`),
-        rua: prompt(`Digite o nome da sua rua`),
-        complemento: prompt(`Digite um complemento`),
-        numeroCasa: prompt(`Digite o numero da sua casa`),
-        pontoDeReferencia: prompt(`Digite um ponto de referencia`)
-    };
-}
 
 let somaTotal = valorBebida[bebida - 1] + valorBordaPizza[bordaPizza - 1] + valorTamanhoPizza[tamanhoPizza - 1] + valorEntrega[tipoEntrega - 1];
 
-switch (numeroSaborPizza, tipoEntrega) {
+let informacoesDelivery = correio(tipoEntrega);
+let tamanho = tamanhoDaPizza(tamanhoPizza);
+let borda = bordasSabor(bordaPizza);
+let sabor = pizzaSaborUnico(saborPizza);
+let meia2 = pizzaSaborDuplo(saborPizza2);
+let acompanhamento = nomeBebida(bebida);
 
-    case numeroSaborPizza == 2 && tipoEntrega == 2: 
-        alert(`O seu pedido foi:\nTamanho: ${tamanhoPizza}.\nO tipo de borda é: ${saborPizza2}`)
-        break;
+let formaPagamento = parseInt(prompt(`O valor total foi do pedido foi: R$${somaTotal},00.\nPara finalizar, nos informe a qual será a forma de pagamento\n1: Cartão (Débito/Crédito)\n2: Pix\n3: PicPay\n4: Dinheiro`));
+
+let pagamento = formaDePagamento(formaPagamento);
+
+if (formaPagamento == 4) {
+    troco = prompt(`Você precisará de troco?\nDigite: sim ou não `).toLowerCase();
+
+    if (troco == "sim") {
+        valorTroco = parseInt(prompt(`O valor total foi de: R$${somaTotal},00.\n${nome}, digite para quanto precisaremos levar o troco`));
+    }
+}
 
 
-let confirmacao = parseInt(prompt(`Deseja confirmar seu pedido?`));
-let certeza = parseInt(prompt(`Tem certeza?`));
 
-let formaPagamento = parseInt(prompt(`1: Cartão\n
-2: Pix\n
-3: PicPay\n
-4: Dinheiro`));
-
+if (numeroSaborPizza == 2) {
+    alert(`O pedido ${numeroDoPedido()} foi:\nTamanho: ${tamanho}.\nO tipo de borda escolhido é: ${borda}\nUma metade será de: ${sabor}\nA outra metade será de: ${meia2}.\nSua bebida é: ${acompanhamento}.\nO tipo de entrega é:${informacoesDelivery}.\nO valor total é: <strong>R$${somaTotal},00</strong>.\nA forma de pagamento será: ${pagamento}.`);
+}
+else if (numeroSaborPizza == 1) {
+    alert(`O pedido ${numeroDoPedido()} foi:\nTamanho: ${tamanho}.\nO tipo de borda escolhido é: ${borda}\nUma metade será de: ${sabor}\nSua bebida é: ${acompanhamento}.\nO tipo de entrega é:${informacoesDelivery}.\nO valor total é: R$${somaTotal},00.\nA forma de pagamento será: ${pagamento}.`);
+}
+else {
+    alert('Você digitou algum dado errado, refaça o pedido!!\nNão desista de nós <3!!');
 
 }
+
+let confirmacao = prompt(`Você deseja confirmar o pedido e os dados mostrados?\nDigite: sim ou não`).toLowerCase();
+
+if(confirmacao != "sim"){
+    alert("Pedido cancelado!!\nNão desista de nós!!\nReinicie a página, por favor!")
+}
+
+document.write(`O número do seu pedido é: ${numeroDoPedido()}`)
+document.write(`Tamanho: ${tamanho}.`)
+document.write(`O tipo de borda escolhido é: ${borda}`)
+document.write(`Uma metade será de: ${sabor}`)
+document.write(`Sua bebida é: ${acompanhamento}`)
+document.write(`O tipo de entrega é:${informacoesDelivery}`)
+document.write(`O valor total é: <strong>R$${somaTotal},00`)
+document.write(`A forma de pagamento será: ${pagamento}.`)
+document.write(`Enviaremos o troco de: R$${valorTroco - somaTotal},00.`)
+
+function tamanhoDaPizza(tamanhoPizza) {
+    let tamanho;
+    switch (tamanhoPizza) {
+        case 1:
+            tamanho = "Pequena (20cm)";
+            break;
+        case 2:
+            tamanho = "Media (25cm)";
+            break;
+        case 3:
+            tamanho = "Gigante (35cm)";
+            break;
+        case 4:
+            tamanho = "Gigante (45cm)";
+            break;
+    }
+    return tamanho
+}
+
+function bordasSabor(bordaPizza) {
+    switch (bordaPizza) {
+
+        case 1: return `Gorgonzola`;
+            break;
+        case 2: return `Provolone`;
+            break;
+        case 3: return `Catupiry`;
+            break;
+        case 4: return `Cheddar`;
+            break;
+        case 5: return `Você não escolheu nenhum sabor para a borda`
+            break;
+    }
+}
+
+function pizzaSaborUnico(saborPizza) {
+    let sabor;
+    switch (saborPizza) {
+        case 1:
+            sabor = "Calabresa";
+            break;
+        case 2:
+            sabor = "4 Queijos";
+            break;
+        case 3:
+            sabor = "Frango";
+            break;
+        case 4:
+            sabor = "Portuguesa";
+            break;
+        case 5:
+            sabor = "Camarão";
+            break;
+        case 6:
+            sabor = "Carne seca";
+            break;
+        case 7:
+            sabor = "Lombo Canadense";
+            break;
+        case 8:
+            sabor = "X-Fritas";
+            break;
+    }return sabor
+}
+
+function pizzaSaborDuplo(saborPizza2) {
+    let sabor2;
+    switch (saborPizza2) {
+        case 1:
+            sabor2 = "Calabresa"
+            break;
+        case 2:
+            sabor2 = "4 Queijos"
+            break;
+        case 3:
+            sabor2 = "Frango"
+            break;
+        case 4:
+            sabor2 = "Portuguesa"
+            break;
+        case 5:
+            sabor2 = "Camarão"
+            break;
+        case 6:
+            sabor2 = "Carne seca"
+            break;
+        case 7:
+            sabor2 = "Lombo Canadense"
+            break;
+        case 8:
+            sabor2 = "X-Fritas"
+            break;
+    }
+    return sabor2
+}
+
+function nomeBebida(bebida) {
+    let nomeDaBebida;
+    switch (bebida) {
+        case 1:
+            nomeDaBebida = "Coca Cola"
+            break;
+        case 2:
+            nomeDaBebida = "Fanta"
+            break;
+        case 3:
+            nomeDaBebida = "Guaraná Antartica"
+            break;
+        case 4:
+            nomeDaBebida = "H2O"
+            break;
+        case 5:
+            nomeDaBebida = "Del Valle"
+            break;
+        case 6:
+            nomeDaBebida = "Sem bebida"
+            break;
+    }
+    return nomeDaBebida
+}
+
+
+function correio(tipoEntrega) {
+    if (tipoEntrega == 2) {
+        telefone = prompt(`Informe o telefone com DDD:\nExemplo: (22) 999999999`);
+        enderecoCompleto = prompt(`Informe o seu endereço com número:\nExemplo: Avenida Alberto Braune, 870`);
+        complemento = prompt(`Tem algum complemento para nos ajudar?`);
+        bairro = prompt(`Informe o seu bairro:`);
+        referencia = prompt(`Tem algum ponto de referência para auxiliar o entregador?`);
+
+        return `Delivery`;
+
+    } else {
+        nomeRetirada = prompt(`Informe o nome de quem irá realizar a retirada do pedido:`);
+        telefoneRetirada = prompt(`Informe um telefone com DDD para retirada do pedido\n Exemplo: (22) 999999999`);
+
+        return `Retirada no balcão`;
+    };
+}
+
+function formaDePagamento(formaPagamento) {
+    let pagamento;
+    switch (formaPagamento) {
+        case 1:
+            pagamento = "Cartão (débito/crédito)"
+            break;
+        case 2:
+            pagamento = "pix"
+            break;
+        case 3:
+            pagamento = "PicPay"
+            break;
+        case 4:
+            pagamento = "Dinheiro"
+            break;
+    }
+    return pagamento
+}
+
+function numeroDoPedido() {
+    return Math.floor(Math.random() * 1001);
+}
+
+
