f82d3abfe493c8c3ff7aa333cf020d6821851952
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-04-08 10:41:39 -0300

    Declaração feita +-

--- index.js
+++ index.js
@@ -28,8 +28,10 @@
 let nome = prompt("Bem vindo, por favor informe seu nome : )!");
 
 let tamanhoPizza = parseInt(prompt(`${nome} 1: Pequena 20cm  - R$25,00\n2: Media 25cm  - R$30,00\n3: Gigante 35cm  - R$40,00\n4: Familia 40cm  - R$45,00` ));
+let valorTamanhoPizza = [ 25,30,40, 45 ];
 
 let bordaPizza = parseInt(prompt(`Tipo de borda:\n1: Gorgonzola  - R$10,00\n2: Provolone  - R$8,00\n3:Catupiry  - R$5,00\n4: Cheddar - R$5,00\n5:Sem borda`));
+let valorBordaPizza = [ 10, 8, 5, 5, 0 ];
 
 let numeroSaborPizza = parseInt(prompt(`${nome} você deseja uma pizza com 1 ou 2 sabores?`));
 
@@ -40,8 +42,9 @@ Portuguesa - Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n
 Camarão - Mussarela, Camarão, Salsinha e Cebolinha\n
 Carne seca - Mussarela, Carne seca e cream cheese\n
 Lombo Canadense - Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n
-X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita\n
+X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita
 `));
+
 
 let saborPizza2 = parseInt(prompt(`Calabresa - Mussarela, Calabresa e cebola \n
 4 Queijos - Mussarela, Cheddar, Parmesão e Prato\n
@@ -50,16 +53,36 @@ Portuguesa - Mussarela, Pimentão, Cebola, Presunto, Ovo, Cebola\n
 Camarão - Mussarela, Camarão, Salsinha e Cebolinha\n
 Carne seca - Mussarela, Carne seca e cream cheese\n
 Lombo Canadense - Mussarela, Lombo Canadense, Tomate, Orégano, Cebola\n
-X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita\n
+X-Fritas - Mussarela, Contra-filé, Presunto, Bacon, Ovo, Tomate, Cebola, Batata Frita
 `));
 
-let bebida    
-let tipoEntrega 
-let dadosEntrega
-let somaTotal
-let confirmacao
-let formaPagamento
+let bebida = parseInt(prompt(`Coca Cola  - R$12,00\n
+Fanta - R$10,00\n
+Guaraná Antarctica  - R$10,00\n
+H2O  - R$8,00\n
+Del Valle  - R$7,00`));
+let valorBebida = [ 12, 10, 10, 8, 7 ];
 
+let tipoEntrega = parseInt(prompt(`Retirada no Balcão - R$0,00\n
+Delivery  - R$5,00
+`));
+let dadosEntrega = {
+    celular: prompt(`Digite o seu número de celular`),
+    bairro: prompt(`Digite o nome do seu bairro`),
+    rua: prompt(`Digite o nome da sua rua`),
+    complemento: prompt(`Digite um complemento`),
+    numeroCasa: prompt(`Digite o numero da sua casa`),
+    pontoDeReferencia: prompt(`Digite um ponto de referencia`)
+};
+
+let somaTotal
+
+let confirmacao 
+
+let formaPagamento = parseInt(prompt(`1: Cartão\n
+2: Pix\n
+3: PicPay\n
+4: Dinheiro`)); 
 
 
 
